3f67addcc780e34ef4ff5c7d7a95c1a345bda26e
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-03 01:22:57 +0500

    Update App and write tests

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      Hello world
+    <div className="container">
+      <div>
+        <h1>First React Application with TDD.</h1>
+      </div>
+      <div>
+          Hello World!
+      </div>
+      <div>
+        My name is <strong>Hassan Ali Khan</strong> 
+        <br/>
+        I am <strong>28</strong> years old, next yaer I will be <strong>29.</strong>
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Header / Title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/First React Application with TDD./i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('Hello', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Hello World!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('Name', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/My name is/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
